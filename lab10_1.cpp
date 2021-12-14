e12ac21bad0ed339b8203cacf92974ff5a3d9e2b
Smart309 <pattharapon_takham@cmu.ac.th>
2021-12-14 15:03:09 +0700

    "UP All R1"

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 
 int main(){	
+	double loan , rate , pay , Total , NewBalance ;
 	cout << "Enter initial loan: ";
+	cin >> loan ;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate ;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay ;
+	NewBalance = loan ;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -18,16 +20,27 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	for (int x = 1; loan > 0; x++)
+	{
+		cout << fixed << setprecision(2);
+		cout << setw(13) << left << x;
+		cout << setw(13) << left << loan;
+		cout << setw(13) << left << loan*rate/100;
+
+		Total =  NewBalance + loan*rate/100 ;
+		NewBalance = Total - pay ;
+		cout << setw(13) << left << Total;
+		if (pay > loan + loan*rate/100)
+		{
+			cout << setw(13) << left << Total ;
+			cout << setw(13) << left << 0.00 ;
+			break ;
+		}else{
+			cout << setw(13) << left << pay ;
+			cout << setw(13) << left << NewBalance ;
+		}
+		cout << "\n";
+		loan = NewBalance;
+	}
 	return 0;
 }
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N , int M)
+{
+	if (N > 0 && M > 0)
+	{
+		for (int i = 0 ; i < N ; i++){
+			for (int I = 0; I < M ; I++)
+			{
+				cout << "O" ;
+			}
+			cout << "\n" ;
+		}
+	}else{ cout << "Invalid input" ; }
+
+}
 
 int main(){
 	
